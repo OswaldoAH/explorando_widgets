5172f29d25797f5e0e67cdd2d7753d1edbcd2a0e
Oswaldo Alvarez <mynor4755@hotmail.com>
2020-05-25 17:45:12 -0600

    1.Se agregó la lógica para navegar en diferentes pantallas. 2. Se configuraron las rutas en main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
  
-import 'src/pages/home_page.dart';
+import 'package:app_ejemplo_widgets/src/pages/home_page.dart';
+import 'package:app_ejemplo_widgets/src/pages/alert_page.dart';
+import 'package:app_ejemplo_widgets/src/pages/avatar_page.dart';
 
 void main() => runApp(MyApp());
  
@@ -10,7 +12,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes APP',
       debugShowCheckedModeBanner: false,
-      home: HomePage()
+      //home: HomePage()
+      initialRoute: '/',
+      routes: <String,WidgetBuilder>{
+        '/':(BuildContext context)=>HomePage(),
+        'alert':(BuildContext context)=> AlertPage(),
+        'avatar' : (BuildContext context)=> AvatarPage(),
+      },
     );
   }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
-import 'package:app_ejemplo_widgets/src/Utils/icon_string_util.dart';
 import 'package:flutter/material.dart';
 
+import 'package:app_ejemplo_widgets/src/pages/alert_page.dart';
+import 'package:app_ejemplo_widgets/src/Utils/icon_string_util.dart';
 import 'package:app_ejemplo_widgets/src/providers/menu_provider.dart';
 
 class HomePage extends StatelessWidget {
@@ -20,20 +21,31 @@ class HomePage extends StatelessWidget {
       initialData: [],
       builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _crearLista(snapshot.data),
+          children: _crearLista(snapshot.data,context),
         );
       },
     );
   }
 
-  List<Widget> _crearLista(List<dynamic> data) {
+  List<Widget> _crearLista(List<dynamic> data,BuildContext context) {
     final List<Widget> opciones = [];
     data.forEach((opt) {
       final widgetTemp = ListTile(
         title: Text(opt['texto']),
         leading: getIcon(opt['icon']),
         trailing: Icon(Icons.arrow_right),
-        onTap: () {},
+        onTap: () {
+          //Forma de navegar por nombres de rutas
+          Navigator.pushNamed(context, opt['ruta']);
+
+          //Forma larga de navegar entre pantallas.
+          // final route = MaterialPageRoute(
+          //   builder: (context){
+          //     return AlertPage();
+          //   });
+          // Navigator.push(context, route);
+
+        },
       );
       opciones..add(widgetTemp)..add(Divider());
     });

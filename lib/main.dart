376add7865aa05bf09ef80b129402ad1bd8440ee
Oswaldo Alvarez <mynor4755@hotmail.com>
2020-05-25 13:49:45 -0600

    Se configuró los archivos para añadir el Json y mostrar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
  
-import 'package:app_ejemplo_widgets/src/pages/home_temp.dart';
+import 'src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
  
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes APP',
       debugShowCheckedModeBanner: false,
-      home: HomePageTemp()
+      home: HomePage()
     );
   }
 }

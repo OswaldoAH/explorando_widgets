6a627ac0f808124fad7351602f6004714dd7e2c5
Oswaldo Alvarez <mynor4755@hotmail.com>
2020-05-26 18:16:34 -0600

    Agregando estilo a un contenedor y a un card

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -15,7 +15,9 @@ class CardPage extends StatelessWidget {
           ),
           children: <Widget>[
             _cardTipo1(),
-            SizedBox(height: 30.0,),
+            SizedBox(
+              height: 30.0,
+            ),
             _cardTipo2(),
           ],
         ),
@@ -25,6 +27,8 @@ class CardPage extends StatelessWidget {
 
   Widget _cardTipo1() {
     return Card(
+      elevation: 10,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       child: Column(
         children: <Widget>[
           ListTile(
@@ -40,9 +44,9 @@ class CardPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
               FlatButton(
-                  onPressed: () {},
-                  child: Text('Cancelar'),
-                ),
+                onPressed: () {},
+                child: Text('Cancelar'),
+              ),
               FlatButton(onPressed: () {}, child: Text('Aceptar')),
             ],
           )
@@ -52,19 +56,38 @@ class CardPage extends StatelessWidget {
   }
 
   Widget _cardTipo2() {
-    return Card(
+    final card = Container(
       child: Column(
         children: <Widget>[
           FadeInImage(
-            image: NetworkImage('https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjI0MX0&w=1000&q=80'),
+            image: NetworkImage(
+                'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjI0MX0&w=1000&q=80'),
             placeholder: AssetImage('assets/Loading.gif'),
             fadeInDuration: Duration(milliseconds: 300),
             height: 250.0,
             fit: BoxFit.cover,
           ),
-          
-          Container(padding: EdgeInsets.symmetric(vertical: 8), child: Text('Algo')),
+          Container(
+              padding: EdgeInsets.symmetric(vertical: 8), child: Text('Algo')),
         ],
+      ),
+    );
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10,
+            offset: Offset(2, 10),
+            spreadRadius: 5
+          )
+        ]
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: card,
       ),
     );
   }
